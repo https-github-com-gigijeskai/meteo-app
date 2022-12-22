476dcae78b6c7dab665d6e61182385b60869337e
gigijeskai <luigindaco@gmail.com>
2022-12-22 17:31:29 +0100

    commit

--- index.js
+++ index.js
@@ -28,3 +28,24 @@ function forecastToggle() {
     y.style.display = "flex";
   }
 }
+
+let weather = {
+  apiKey: "b3986ed7dbefd400d6ae3068ccccdc5d",
+  fetchWeather: function (city) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey + "&lang=it")
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, temp_min, temp_max, humidity } = data.main;
+    const { speed } = data.wind;
+    const { country, sunrise, sunset } = data.sys;
+    console.log(name, icon, description, temp, temp_min, temp_max, humidity, speed, country, sunrise, sunset);
+    document.querySelector(".city").innerText = "Meteo di" + " " + name;
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".weather-conditions").innerHTML = description;
+    document.querySelector(".temp").innerHTML = Math.round(temp) + "&#176 C";
+  },
+};
