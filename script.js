5ca525562ba54dee6af5cbdcc33a5ddd7f6ebaf2
gigijeskai <luigindaco@gmail.com>
2023-01-05 17:17:04 +0100

    commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function hamburgerMenu() {
   let x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
+  if (x.style.display === "flex") {
     x.style.display = "none";
   } else {
-    x.style.display = "block";
+    x.style.display = "flex";
   }
 }
 function detailToggle() {
